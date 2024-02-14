f1e39bc7c896ce6f9b5820eed9d7b6f2643b0d44
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2024-02-14 20:31:59 +0000

    Flask-second-commit

--- src/hello.py
+++ src/hello.py
@@ -1,11 +1,25 @@
 from flask import Flask, request, render_template
+import pickle as pickle
 from pickle import load
+import numpy as np
+import numpy as numpy
+import joblib as joblib
+import os
+import pickle
 
-app = Flask(__name__)
+print(os.getcwd())
+
+"""model=joblib.load("iris_model_LR.pkl")
+data = [[1.0, 2.0, 3.0, 4.0]]
+prediction = model.predict(data)
+print(prediction)"""
+
+"""app = Flask(__name__)
 app.config['TEMPLATES_FOLDER'] = '/workspaces/ML-WEB-APP-USING-FLASK/src/template/index.html'
 
 
-model = load(open("/workspaces/ML-WEB-APP-USING-FLASK/models/iris_model_LR.pkl", "rb"))
+#model = load(open("/workspaces/ML-WEB-APP-USING-FLASK/models/iris_model_LR.pkl", "rb"))
+model = pickle.load(open("/workspaces/ML-WEB-APP-USING-FLASK/models/decision_tree_classifier_default_42.sav", "rb"))
 class_dict = {
     "0": "Iris setosa",
     "1": "Iris versicolor",
@@ -22,7 +36,7 @@ def index():
         
         data = [[val1, val2, val3, val4]]
         prediction = str(model.predict(data)[0])
-        pred_class = class_dict.get(prediction, "Unknown")
+        pred_class = class_dict[prediction]
     else:
         pred_class = None
     
@@ -30,3 +44,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
