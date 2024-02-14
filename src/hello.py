98ad64730cce08b371dd1e05f17b706477a05a9e
Luis Penafiel Palmer <135802985+LuisPenafiel@users.noreply.github.com>
2024-02-14 19:22:40 +0000

    Flas-Firs-commit

---
+++ src/hello.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from pickle import load
+
+app = Flask(__name__)
+app.config['TEMPLATES_FOLDER'] = '/workspaces/ML-WEB-APP-USING-FLASK/src/template/index.html'
+
+
+model = load(open("/workspaces/ML-WEB-APP-USING-FLASK/models/iris_model_LR.pkl", "rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica"
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        val1 = float(request.form['val1'])
+        val2 = float(request.form['val2'])
+        val3 = float(request.form['val3'])
+        val4 = float(request.form['val4'])
+        
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict.get(prediction, "Unknown")
+    else:
+        pred_class = None
+    
+    return render_template('index.html', prediction=pred_class)
+
+if __name__ == '__main__':
+    app.run(debug=True)
